106deea6ad1b13899913575f0bb17f5f7e31db35
Kevin Tseng <kevintsengtw@gmail.com>
2014-06-16 14:18:15 +0800

    Sample_EF6_Logging_Intercepting
    ASP.NET MVC 5 with Entity Framework 6 and NLog, use EF6 Logging and
    Intercepting

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 twMVC-15-2
 ==========
 
-2014-06-14 twMVC#15 下半場範例程式
+201406-14 twMVC#15
+
+開發效能與效率
+
+講者：Kevin Tseng（Microsoft MVP 2013,2014、twMVC核心講者、SkillTree 專任講師）
+
+課程簡介：
+
+在系統開發時往往會被忽略的就是效能問題，而且通常都是在最後上線前才想到要去做效能調校，這次將為各位介紹如何透過工具的使用讓我們在開發期間就可以觀察與調校系統效能。
+除了使用工具去觀察效能之外，也將會介紹如何利用工具來幫助我們提高開發的效率。
+
+範例程式說明：
+
+MVC5_EF5 - ASP.NET MVC 5 with Entity Framework 5, use Clutch.Diagnostics.EntityFramework
+
+Sample_EF6_Logging_Intercepting - ASP.NET MVC 5 with Entity Framework 6 and NLog, use EF6 Logging and Intercepting
+
+
+
+
+開發環境：Visual Studio 2013
+
+為了避免檔案過大，因此使用的套件皆無上傳，請自行還原，還原方式請參考 http://demo.tc/Post/763
---
+++ Sample_EF6_Logging_Intercepting/WebApplication1/Infrastructure/Interceptor/NLogCommandInterceptor.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Web;
+using NLog;
+
+namespace WebApplication1.Infrastructure.Interceptor
+{
+    public class NLogCommandInterceptor : IDbCommandInterceptor
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        protected internal Stopwatch Stopwatch
+        {
+            get { return _stopwatch; }
+        }
+
+
+        public void NonQueryExecuting(
+            DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            LogIfNonAsync(command, interceptionContext);
+            Stopwatch.Restart();
+        }
+
+        public void NonQueryExecuted(
+            DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            Stopwatch.Stop();
+            LogNonQueryCommand(command, interceptionContext);
+        }
+
+        public void ReaderExecuting(
+            DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            //LogIfNonAsync(command, interceptionContext);
+            Stopwatch.Restart();
+        }
+
+        public void ReaderExecuted(
+            DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            if (interceptionContext.Exception != null)
+            {
+                LogIfError(command, interceptionContext);
+            }
+            else
+            {
+                Stopwatch.Stop();
+                string logContent = LogCommand(command, interceptionContext);
+                Logger.Trace(logContent);
+            }
+        }
+
+        public void ScalarExecuting(
+            DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            //LogIfNonAsync(command, interceptionContext);
+            Stopwatch.Restart();
+        }
+
+        public void ScalarExecuted(
+            DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            if (interceptionContext.Exception != null)
+            {
+                LogIfError(command, interceptionContext);
+            }
+            else
+            {
+                Stopwatch.Stop();
+                string logContent = LogCommand(command, interceptionContext);
+                Logger.Trace(logContent);
+            }
+        }
+
+        //=========================================================================================
+
+        private void LogIfNonAsync<TResult>(
+            DbCommand command, DbCommandInterceptionContext<TResult> interceptionContext)
+        {
+            if (!interceptionContext.IsAsync)
+            {
+                Logger.Trace("Non-async command used: {0}", command.CommandText);
+            }
+        }
+
+        private void LogIfError<TResult>(
+            DbCommand command, DbCommandInterceptionContext<TResult> interceptionContext)
+        {
+            if (interceptionContext.Exception != null)
+            {
+                Logger.Error("Command {0} failed with exception {1}",
+                    command.CommandText, interceptionContext.Exception);
+            }
+        }
+
+        private void LogNonQueryCommand<TResult>(
+            DbCommand command,
+            DbCommandInterceptionContext<TResult> interceptionContext)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException("command");
+            }
+            if (interceptionContext == null)
+            {
+                throw new ArgumentNullException("interceptionContext");
+            }
+
+            if (interceptionContext.Exception != null)
+            {
+                LogIfError(command, interceptionContext);
+            }
+            else
+            {
+                //只有針對不是非同步處理
+                if (!interceptionContext.IsAsync)
+                {
+                    string logContent = LogCommand(command, interceptionContext);
+                    Logger.Trace(logContent);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Logs the command.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="command">The command.</param>
+        /// <param name="interceptionContext">The interception context.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// command
+        /// or
+        /// interceptionContext
+        /// </exception>
+        private string LogCommand<TResult>(
+            DbCommand command,
+            DbCommandInterceptionContext<TResult> interceptionContext)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException("command");
+            }
+            if (interceptionContext == null)
+            {
+                throw new ArgumentNullException("interceptionContext");
+            }
+
+            var commandText = command.CommandText ?? "<null>";
+
+            if (!commandText.EndsWith(Environment.NewLine, StringComparison.Ordinal))
+            {
+                commandText = string.Concat(commandText, Environment.NewLine);
+            }
+
+            commandText = string.Concat(
+                commandText, Environment.NewLine,
+                "Parameters: ", Environment.NewLine);
+
+            foreach (var parameter in command.Parameters.OfType<DbParameter>())
+            {
+                commandText = string.Concat(
+                    commandText,
+                    LogParameter(command, interceptionContext, parameter));
+            }
+
+            //執行開始時間
+            commandText = string.Concat(commandText, Environment.NewLine,
+                "-- Executing at ", DateTimeOffset.Now.ToLocalTime());
+
+            //取得目前執行個體所測量的已耗用時間總和，以毫秒為單位
+
+            long z = 10000 * Stopwatch.ElapsedTicks;
+            decimal timesTen = (int)(z / Stopwatch.Frequency);
+            var elapsedMilliseconds = timesTen / 10;
+
+            commandText = string.Concat(commandText, Environment.NewLine,
+                "-- Completed in ", elapsedMilliseconds, " ms");
+
+            return commandText;
+        }
+
+        private string LogParameter<TResult>(
+            DbCommand command,
+            DbCommandInterceptionContext<TResult> interceptionContext,
+            DbParameter parameter)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException("command");
+            }
+            if (interceptionContext == null)
+            {
+                throw new ArgumentNullException("interceptionContext");
+            }
+            if (parameter == null)
+            {
+                throw new ArgumentNullException("parameter");
+            }
+
+            // -- Name: [Value] (Type = {}, Direction = {}, IsNullable = {}, Size = {}, Precision = {} Scale = {})
+            var builder = new StringBuilder();
+            builder.Append("-- ")
+                .Append(parameter.ParameterName)
+                .Append(": '")
+                .Append((parameter.Value == null || parameter.Value == DBNull.Value) ? "null" : parameter.Value)
+                .Append("' (Type = ")
+                .Append(parameter.DbType);
+
+            if (parameter.Direction != ParameterDirection.Input)
+            {
+                builder.Append(", Direction = ").Append(parameter.Direction);
+            }
+            if (!parameter.IsNullable)
+            {
+                builder.Append(", IsNullable = false");
+            }
+            if (parameter.Size != 0)
+            {
+                builder.Append(", Size = ").Append(parameter.Size);
+            }
+            if (((IDbDataParameter)parameter).Precision != 0)
+            {
+                builder.Append(", Precision = ").Append(((IDbDataParameter)parameter).Precision);
+            }
+            if (((IDbDataParameter)parameter).Scale != 0)
+            {
+                builder.Append(", Scale = ").Append(((IDbDataParameter)parameter).Scale);
+            }
+
+            builder.Append(")").Append(Environment.NewLine);
+
+            return builder.ToString();
+        }
+
+    }
+}
